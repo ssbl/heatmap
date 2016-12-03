11f2a10264b5bc5d8f6165e15720c67f2ab6d4db
Shubham Lagwankar <shubhu105@gmail.com>
2016-12-02 20:10:37 -0500

    default to current directory

--- heatmap.c
+++ heatmap.c
@@ -8,20 +8,26 @@
 int
 main(int argc, char *argv[])
 {
+    char *dir;
     DIR *dirp;
 
-    if (argc != 2) {
+    if (argc > 2) {
         fprintf(stderr, "Usage: heatmap [DIR]\n");
         exit(EXIT_FAILURE);
+    } else if (argv[1]) {
+        dir = argv[1];
+    } else {
+        dir = ".";
     }
 
-    if ((dirp = opendir(argv[1])) == NULL) {
+    if ((dirp = opendir(dir)) == NULL) {
         perror("opendir");
         exit(EXIT_FAILURE);
-    } else                      /* directory exists */
+    } else {                    /* directory exists */
         closedir(dirp);
+    }
 
-    classify_and_print(argv[1], NOW);
+    classify_and_print(dir, NOW);
 
     return 0;
 }
